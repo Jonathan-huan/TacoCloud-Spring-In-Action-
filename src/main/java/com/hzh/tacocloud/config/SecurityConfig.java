49bab7ece98e3fa463610975eb9ae953baba162f
zhaohuan.huang <1322329153@qq.com>
2024-02-14 20:44:09 +0800

    [ADD]K8S持续交付

--- src/main/java/com/hzh/tacocloud/config/SecurityConfig.java
+++ src/main/java/com/hzh/tacocloud/config/SecurityConfig.java
@@ -4,42 +4,68 @@ import com.hzh.tacocloud.domain.entity.User;
 import com.hzh.tacocloud.domain.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
-@EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+//    @Bean
+//    public UserDetailsService userDetailsService(UserRepository userRepository) {
+//        return username -> {
+//            User user = userRepository.findByUsername(username);
+//            if(user!=null)return user;
+//            throw new UsernameNotFoundException("User '"+username+"' not found");
+//        };
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
+    @Profile("dev")
+    public ReactiveUserDetailsService userDetailsService(UserRepository userRepository){
         return username -> {
             User user = userRepository.findByUsername(username);
-            if(user!=null)return user;
+            if(user!=null)return Mono.just(user);
             throw new UsernameNotFoundException("User '"+username+"' not found");
         };
     }
 
     @Bean
+    @Profile("dev")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(
                 registry -> registry
                         .requestMatchers("/design","/orders").hasRole("USER")
                         .requestMatchers("/","/**").permitAll())
-                        .csrf(configurer -> configurer.ignoringRequestMatchers("/h2-console/**"))
+                        .csrf(configurer -> configurer.ignoringRequestMatchers("/h2-console/**").disable()) //开发环境禁用csrf保护
                         .headers(configurer -> configurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                         .formLogin(configurer -> configurer.loginPage("/login")
                 ).build();
     }
+
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
+//        return http.authorizeExchange(
+//                spec->spec
+//                        .pathMatchers("/api/tacos","orders")
+//                        .hasAuthority("USER")
+//                        .anyExchange()
+//                        .permitAll()
+//                ).build();
+//    }
 }
--- src/test/java/com/hzh/tacocloud/TacoCloudApplicationTests.java
+++ src/test/java/com/hzh/tacocloud/TacoCloudApplicationTests.java
@@ -4,18 +4,21 @@ import com.google.common.collect.Maps;
 import org.apache.commons.collections4.CollectionUtils;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 //@SpringBootTest
 class TacoCloudApplicationTests {
 	@Test
-	void collectionTest() throws JSONException {
-		HashMap<Object, Object> map = Maps.newHashMap();
-		HashMap<Object, Object> map1 = new HashMap<>();
+	void collectionTest() {
 	}
 }
